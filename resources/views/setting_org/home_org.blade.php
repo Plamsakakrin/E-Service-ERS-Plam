ee3a35f36afb7578f39854ab0346ac6020f2b271
Plamsakakrin <61239507+Plamsakakrin@users.noreply.github.com>
2021-06-11 17:44:37 +0700

    update

--- resources/views/setting_org/home_org.blade.php
+++ resources/views/setting_org/home_org.blade.php
@@ -8,42 +8,28 @@
     </div>
     <hr>
     <div class="row">
-        <!-- <div class="col-12">
-                <div class="card">
-                    <div class="card-body">
-                    <a href="{{route('searchbig_org')}}" class="btn btn-link">องค์การใหญ่</a>
-                    <a href="#" class="btn btn-link">องค์การย่อย</a>
-                    </div>
-                </div>
-            </div> -->
-
-
         <div class="container">
             <div class="row">
-                <div class="col-md-6">
-                        <a href="{{route('searchbig_org')}}">
-                    <div class="cardd card-1" style="height:120px; text-align:center; border: none;">
-                        <!-- <i class="fa fa-file-video-o" aria-hidden="true" style="font-size:100px; background-color:orang;"></i> -->
-                        <!-- <hr> -->
-                        <br>
+                <div class="col-md-6 mb-4 pl-2">
+                    <a href="{{route('searchbig_org')}}">
+                        <div class="cardd card-1" style="height:140px; text-align:center; border: none;">
+                            <!-- <i class="fa fa-file-video-o" aria-hidden="true" style="font-size:100px; background-color:orang;"></i> -->
+                            <!-- <hr> -->
+                            <br>
                             <h3>องค์การทางศาสนา <br>(องค์การใหญ่)</h3>
-                    </div>
-                        </a>
+                        </div>
+                    </a>
                 </div>
-                <div class="col-md-6">
-                        <a href="{{route('searchsub_org')}}">
-                    <div class="cardd card-3" style="height:120px; text-align:center; border: none;">
+                <div class="col-md-6 mb-4 pl-2">
+                    <a href="{{route('searchsub_org')}}">
+                        <div class="cardd card-3" style="height:140px; text-align:center; border: none;">
                             <br>
                             <h3>องค์การทางศาสนา <br>(องค์การย่อย)</h3>
-                    </div>
-                        </a>
+                        </div>
+                    </a>
                 </div>
-
             </div>
         </div>
-
-
-
     </div>
 
 </div>
